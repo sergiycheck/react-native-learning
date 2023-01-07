68b4666bfae57fbe2d98322a3c7e28344e3ecc30
serhii <sergiychech@gmail.com>
2023-01-07 15:24:11 +0200

    navigation lifecycle

--- learningReactNativeNavigation/index.js
+++ learningReactNativeNavigation/index.js
@@ -1,10 +1,13 @@
 import {AppRegistry} from 'react-native';
 
-import App from './App';
+// import App from './App';
 // import App from './AppWithDrawer';
-
-import {App_NestingMultiple} from './src/components/fundamentals/nestingNavigators/NestingMultiple';
+// import {App_NestingMultiple} from './src/components/fundamentals/nestingNavigators/NestingMultiple';
 
 import {name as appName} from './app.json';
+import AppWithGlobalTabAndNestedStackNavigation from './src/components/fundamentals/nestingNavigators/TabWithNestedStackNavigation';
 
-AppRegistry.registerComponent(appName, () => App_NestingMultiple);
+AppRegistry.registerComponent(
+  appName,
+  () => AppWithGlobalTabAndNestedStackNavigation,
+);
---
+++ learningReactNativeNavigation/src/components/fundamentals/nestingNavigators/TabWithNestedStackNavigation.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import {Button, View, Text} from 'react-native';
+import {
+  NavigationContainer,
+  NavigationProp,
+  useFocusEffect,
+} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+function SettingsScreen({navigation}: {navigation: NavigationProp<any, any>}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Settings Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({navigation}: {navigation: NavigationProp<any, any>}) {
+  React.useEffect(
+    () =>
+      navigation.addListener('focus', () => console.log('Screen was focused')),
+    [navigation],
+  );
+
+  React.useEffect(
+    () =>
+      navigation.addListener('blur', () => console.log('Screen was unfocused')),
+    [navigation],
+  );
+
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log('useFocusEffect focused');
+
+      return () => {
+        // Do something when the screen is unfocused
+        console.log('useFocusEffect unfocused');
+      };
+    }, []),
+  );
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+    </View>
+  );
+}
+
+function HomeScreen({navigation}: {navigation: NavigationProp<any, any>}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({
+  navigation,
+}: {
+  navigation: NavigationProp<any, any> | any;
+}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+const SettingsStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+
+export default function AppWithGlobalTabAndNestedStackNavigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="First">
+          {() => (
+            <SettingsStack.Navigator>
+              <SettingsStack.Screen
+                name="Settings"
+                component={SettingsScreen}
+              />
+              <SettingsStack.Screen name="Profile" component={ProfileScreen} />
+            </SettingsStack.Navigator>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Second">
+          {() => (
+            <HomeStack.Navigator>
+              <HomeStack.Screen name="Home" component={HomeScreen} />
+              <HomeStack.Screen name="Details" component={DetailsScreen} />
+            </HomeStack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
