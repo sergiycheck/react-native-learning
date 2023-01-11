df2699bfaaad53aa97d96dbc8ad641b3526dade1
serhii <sergiychech@gmail.com>
2023-01-11 22:37:52 +0200

    modified emu_osx.sh

--- emu_osx.sh
+++ emu_osx.sh
@@ -5,12 +5,15 @@
 
 
 docker run -it \
-    -e RAM=8 \
-    -e CORES=2 \
     --device /dev/kvm \
+    -e RAM=8 \
+    -e SMP=4 \
+    -e CORES=4 \
+    -e EXTRA='-usb -device usb-host,hostbus=1,hostport=1' \
+    -e INTERNAL_SSH_PORT=23 \
     -p 50922:10022 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
-    sickcodes/docker-osx:latest
-
-# docker build -t docker-osx .
+    -e GENERATE_UNIQUE=true \
+    -e MASTER_PLIST_URL='https://raw.githubusercontent.com/sickcodes/osx-serial-generator/master/config-custom.plist' \
+    sickcodes/docker-osx:monterey
