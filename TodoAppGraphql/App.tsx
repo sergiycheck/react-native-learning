7060f69eb61af047a94ef307c01aa1380063a5ac
serhii <sergiychech@gmail.com>
2023-02-19 19:00:37 +0200

    used helper relayStylePagination for fiels

--- TodoAppGraphql/App.tsx
+++ TodoAppGraphql/App.tsx
@@ -10,8 +10,8 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import {Todos} from './src/Todos';
-import {IEdgeType, IPaginatedType, ResponseTodo} from './src/types';
 import {TextCenter} from './src/shared-components';
+import {relayStylePagination} from '@apollo/client/utilities';
 
 const client = new ApolloClient({
   uri: 'http://10.0.2.2:3024/graphql',
@@ -19,72 +19,12 @@ const client = new ApolloClient({
     typePolicies: {
       Query: {
         fields: {
-          queryCursorBasedPaginated: {
-            // keyArgs array, which contains the names
-            // of all arguments that the cache should include in its storage keys
-            // don't include limit, prevPageCursor and nextPageCursor
-            keyArgs: false,
-            merge(
-              existing: IPaginatedType<ResponseTodo> | undefined,
-              incoming: IPaginatedType<ResponseTodo>,
-              {args, readField},
-            ) {
-              const {nextPageCursor} = args;
-
-              const merged = existing ? existing.edges.slice(0) : [];
-              let offset = offsetFromCursor(merged, nextPageCursor, readField);
-              // If we couldn't find the cursor, default to appending to
-              // the end of the list, so we don't lose any data.
-              if (offset < 0) offset = merged.length;
-              // Now that we have a reliable offset, the rest of this logic
-              // is the same as in offsetLimitPagination.
-              for (let i = 0; i < incoming.edges.length; ++i) {
-                merged[offset + i] = incoming.edges[i];
-              }
-              return {
-                edges: merged,
-                pageInfo: incoming.pageInfo,
-              };
-            },
-
-            // If you always want to return the whole list, you can omit
-            // this read function.
-            // weird somehow nextPageCursor is undefined
-            // dispite the fact that we are setting it in fetchMore
-          },
+          queryCursorBasedPaginated: relayStylePagination(),
         },
       },
     },
   }),
 });
-
-function offsetFromCursor(
-  items: IEdgeType<ResponseTodo>[],
-  cursor: string,
-  readField: Function,
-) {
-  // Search from the back of the list because the cursor we're
-  // looking for is typically the ID of the last item.
-
-  for (let i = items.length - 1; i >= 0; --i) {
-    const item = items[i];
-
-    // Using readField works for both non-normalized objects
-    // (returning item.id) and normalized references (returning
-    // the id field from the referenced entity object), so it's
-    // a good idea to use readField when you're not sure what
-    // kind of elements you're dealing with.
-
-    if (readField('cursor', item) === cursor) {
-      // Add one because the cursor identifies the item just
-      // before the first item in the page we care about.
-      return i + 1;
-    }
-  }
-
-  // Report that the cursor could not be found.
-  return -1;
-}
 
 const RootComponent = () => {
   const isDarkMode = useColorScheme() === 'dark';
